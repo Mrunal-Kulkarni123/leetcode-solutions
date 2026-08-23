76276b40027a40ee5b9264f532de81dabc37bf90
Mrunal Kulkarni <kmrunal2910@gmail.com>
2026-08-23 11:55:41 +0530

    min cost climbing stairs

---
+++ Dynamic Programming/minCostClimbingStairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+
+var minCostClimbingStairs = function (cost) {
+  let dp = [0, 0];
+  n = cost.length;
+  for (let i = 2; i <= cost.length; i++) {
+    if (!dp[i]) {
+      dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    }
+  }
+  return dp[n];
+};
